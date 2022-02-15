b44a13ac451dc9fbc095ceb32299d52e2a94c532
Pinto Manuel <pintoydc@gmail.com>
2022-02-15 17:34:00 +0200

    First commit

---
+++ src/components/Expenses/Expenses.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import './Expenses.css';
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+
+  /* This function is filtering the year  */
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear
+  })
+
+
+  /* Another way of rendering the condition */
+  /*   let expensesContent = <p>No expenses found</p>
+  if (filteredExpenses.length > 0){
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+    ))
+  } */
+
+  return (
+    <div>
+      <Card className='expenses'>
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+
+
+        {/* Outputing conditional content */}
+        
+        {filteredExpenses.length === 0 ? (
+          <p style={{color: '#fff'}}>
+            No expense found
+          </p>
+        ) : (
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))
+        )}
+        
+        {/* Passing the expense content here */}
+        {/* {expensesContent} */}
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
